8a4f91d39e02b2efeae73c9fc3987626d4a99cb3
Ami <amirouche@gmail.com>
2019-02-20 22:57:59 +0100

    creation de layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,10 +5,8 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+import('../sass/app.scss');
+import('@fortawesome/fontawesome-free/css/all.min.css');
+import('@fortawesome/fontawesome-free/js/all.js');
+import('./sbadmin.js');
+console.log('hello word');
